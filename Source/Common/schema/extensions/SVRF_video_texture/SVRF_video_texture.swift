e7f247d791193e2d45b756a3cde5dc951dfc8b26
Artem Titoulenko <artem@svrf.com>
2019-05-30 18:47:52 -0400

    SVRF_video_texture extension

---
+++ Source/Common/schema/extensions/SVRF_video_texture/SVRF_video_texture.swift
@@ -0,0 +1,31 @@
+//
+//  SVRF_video_texture.swift
+//  Pods-FaceFilter Studio
+//
+//  Created by Artem on 5/30/19.
+//
+
+import Foundation
+import SceneKit
+
+struct GLTFSvrfVideoTextureExtension: GLTFCodable {
+    struct GLTFSvrfVideoTextureSchema: Codable {
+        var texture: Int
+
+        private enum CodingKeys: String, CodingKey {
+            case texture
+        }
+    }
+    
+    let data: GLTFSvrfVideoTextureSchema?
+    enum CodingKeys: String, CodingKey {
+        case data = "SVRF_video_texture"
+    }
+    
+    func didLoad(by object: Any, unarchiver: GLTFUnarchiver) {
+        guard let data = self.data else { return }
+        guard let material = object as? SCNMaterial else { return }
+        
+        try! unarchiver.setTexture(index: data.texture, to: material.diffuse)
+    }
+}
