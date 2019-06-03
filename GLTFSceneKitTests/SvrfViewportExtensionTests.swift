55b391fd21767f994203c3bf206d3b939e0cc11e
Jesse Boyes <jexe-github@jexe.net>
2019-06-03 17:44:37 -0400

    Add test coverage for alignment parsing

--- GLTFSceneKitTests/SvrfViewportExtensionTests.swift
+++ GLTFSceneKitTests/SvrfViewportExtensionTests.swift
@@ -10,24 +10,51 @@ import XCTest
 
 class SvrfViewportExtensionTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testParseHorizontalAlignment_simple() {
+        let (align, percent) = SvrfViewportBindingExtension
+            .parseHorizontalAlignment("center")
+        XCTAssertEqual(align, .center)
+        XCTAssertEqual(percent, 0)
+        
     }
+    
+    func testParseHorizontalAlignment_positiveOffset() {
+        let (align2, percent2) = SvrfViewportBindingExtension
+            .parseHorizontalAlignment("Left + 10%")
+        XCTAssertEqual(align2, .left)
+        XCTAssertEqual(percent2, CGFloat(0.10))
+    }
+    
+    func testParseHorizontalAlignment_negativeOffset() {
+        let (align3, percent3) = SvrfViewportBindingExtension
+            .parseHorizontalAlignment("right-50%")
+        XCTAssertEqual(align3, .right)
+        XCTAssertEqual(percent3, CGFloat(-0.50))
+    }
+    
+    func testParseVerticalAlignment_simple() {
+        let (align, percent) = SvrfViewportBindingExtension
+            .parseVerticalAlignment("top")
+        XCTAssertEqual(align, .top)
+        XCTAssertEqual(percent, 0)
+    }
+    
+    func testParseVerticalAlignment_positiveOffset() {
+        let (align2, percent2) = SvrfViewportBindingExtension
+            .parseVerticalAlignment("bottom + 10%")
+        XCTAssertEqual(align2, .bottom)
+        XCTAssertEqual(percent2, CGFloat(0.10))
+    }
+    
+    func testParseVerticalAlignment_negativeOffset() {
+        let (align3, percent3) = SvrfViewportBindingExtension
+            .parseVerticalAlignment("center-32%")
+        XCTAssertEqual(align3, .center)
+        XCTAssertEqual(percent3, CGFloat(-0.32))
+    }
+    
+    
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
 }
